859c2e7c1dd71ab4d682092714dcdcb082d199fe
Matthew Chenette <matthew.chenette@chevron.com>
2020-09-15 16:37:07 -0500

    barebones example w print

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Distributed, AzManagers, Test
 
 @testset "AzManagers" begin
     @test true
-    print(">>> ", ENV.FIRST_NAME, " <<<")
+    print(">>> ", ENV["FIRST_NAME"], " <<<")
     # write(stdout, "env.FIRST_NAME")
     # write(stdout, "$FIRST_NAME")
 end
