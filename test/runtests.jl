1b37c337e7613253af70e1da75a3fedb2e14e8b8
Matthew Chenette <matthew.chenette@chevron.com>
2020-09-15 16:21:01 -0500

    barebones example w print

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using Distributed, AzManagers, Test
 
 @testset "AzManagers" begin
     @test true
-    write(stdout, "${{ env.FIRST_NAME }}")
+    write(stdout, "env.FIRST_NAME")
+    write(stdout, "$FIRST_NAME")
 end
