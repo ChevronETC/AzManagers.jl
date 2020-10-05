f8fe3294f2ed325a3a847095362389862db491b2
Matthew Chenette <matthew.chenette@chevron.com>
2020-10-05 13:44:21 -0500

    testing always

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,5 @@
 using Distributed, AzManagers, Test
 
 @testset "AzManagers" begin
-    @test true
-    print(">>> ", ENV["subscription_id"], " <<<")
-    print(">>> ", ENV["resource_group"], " <<<")
+    @test false
 end
