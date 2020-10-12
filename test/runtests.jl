d3f3ddb37e066416ec3748aa8cbd51ad8ec5901e
Matthew Chenette <matthew.chenette@chevron.com>
2020-10-12 09:28:12 -0500

    barebones test

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,84 +51,84 @@ write(stdout, "This is: $new")
 #         @test master != worker
 #     end
 
-    #
-    # Unit Test 5 - Verify that the cloud-init startup script ran successfully
-    #
-    for i = 1:tppi
-        @test remotecall_fetch(isfile, workers()[i], ".git-credentials")
-    end
+#     #
+#     # Unit Test 5 - Verify that the cloud-init startup script ran successfully
+#     #
+#     for i = 1:tppi
+#         @test remotecall_fetch(isfile, workers()[i], ".git-credentials")
+#     end
 
-    #
-    # Unit Test 6 - Delete the Julia processes, scale set instances and the scale set itself
-    #
+#     #
+#     # Unit Test 6 - Delete the Julia processes, scale set instances and the scale set itself
+#     #
 
-    # First, verify that the scale set is present
-    session = AzSession()
-    _r = HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
-    @test _r.status == 200
+#     # First, verify that the scale set is present
+#     session = AzSession()
+#     _r = HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
+#     @test _r.status == 200
 
-    @info "Deleting cluster..."
-    rmprocs(workers())
+#     @info "Deleting cluster..."
+#     rmprocs(workers())
 
-    # Last, verify that the scale set has been deleted
-    while true
-        try
-            session = AzSession()    
-            HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
-        catch _e
-            e = JSON.parse(String(_e.response.body))
-            if _e.status == 404 && e["error"]["code"] == "ResourceNotFound"
-                @info "Cluster deleted!"
-                break
-            end
-        end
-        sleep(5)
-    end
-end
-
-# Needs work - Default VM sku causes a failure here
-# @testset "AzManagers, addproc" for kwargs in ( (basename = "test$(randstring('a':'z',4))"), )
-#     addproc(template; basename=kwargs.basename)
+#     # Last, verify that the scale set has been deleted
+#     while true
+#         try
+#             session = AzSession()    
+#             HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
+#         catch _e
+#             e = JSON.parse(String(_e.response.body))
+#             if _e.status == 404 && e["error"]["code"] == "ResourceNotFound"
+#                 @info "Cluster deleted!"
+#                 break
+#             end
+#         end
+#         sleep(5)
+#     end
 # end
 
-@testset "AzManagers, detach" for kwargs in ( (session = AzSession()), )
+# # Needs work - Default VM sku causes a failure here
+# # @testset "AzManagers, addproc" for kwargs in ( (basename = "test$(randstring('a':'z',4))"), )
+# #     addproc(template; basename=kwargs.basename)
+# # end
 
-    #
-    # Unit Test 1 - Create a detached job and persist the server
-    #
-    job1 = @detach vm(;session=kwargs.session, persist=true) begin
-        write(stdout, "job1 - stdout string")
-        write(stderr, "job1 - stderr string")
-    end
+# @testset "AzManagers, detach" for kwargs in ( (session = AzSession()), )
 
-    #
-    # Unit Test 2 - Send a new job to the server started above
-    #
-    job2 = @detachat job1.vm["ip"] begin
-        write(stdout, "job2 - stdout string")
-        write(stderr, "job2 - stderr string")
-    end
+#     #
+#     # Unit Test 1 - Create a detached job and persist the server
+#     #
+#     job1 = @detach vm(;session=kwargs.session, persist=true) begin
+#         write(stdout, "job1 - stdout string")
+#         write(stderr, "job1 - stderr string")
+#     end
 
-    # Wait for jobs to finish
-    wait(job1)
-    wait(job2)
+#     #
+#     # Unit Test 2 - Send a new job to the server started above
+#     #
+#     job2 = @detachat job1.vm["ip"] begin
+#         write(stdout, "job2 - stdout string")
+#         write(stderr, "job2 - stderr string")
+#     end
 
-    @test status(job1) == "done"
-    @test read(job1;stdio=stdout) == "job1 - stdout string"
-    @test read(job1;stdio=stderr) == "job1 - stderr string"
+#     # Wait for jobs to finish
+#     wait(job1)
+#     wait(job2)
 
-    @test status(job2) == "done"
-    @test read(job2;stdio=stdout) == "job2 - stdout string"
-    @test read(job2;stdio=stderr) == "job2 - stderr string"
+#     @test status(job1) == "done"
+#     @test read(job1;stdio=stdout) == "job1 - stdout string"
+#     @test read(job1;stdio=stderr) == "job1 - stderr string"
 
-    #
-    # Unit Test 3 - shut-down the detached server
-    #
-    rmproc(job1.vm)
+#     @test status(job2) == "done"
+#     @test read(job2;stdio=stdout) == "job2 - stdout string"
+#     @test read(job2;stdio=stderr) == "job2 - stderr string"
 
-    #
-    # Unit Test 4 - create a new job on a new detached server that auto-destructs upon completion of its work
-    #
-    job3 = @detach vm(;session=kwargs.session, persist=false) begin
-    end
-end
+#     #
+#     # Unit Test 3 - shut-down the detached server
+#     #
+#     rmproc(job1.vm)
+
+#     #
+#     # Unit Test 4 - create a new job on a new detached server that auto-destructs upon completion of its work
+#     #
+#     job3 = @detach vm(;session=kwargs.session, persist=false) begin
+#     end
+# end
