c922d1ee4586ddea3f6e0e30352ead5043addc3e
Matthew Chenette <matthew.chenette@chevron.com>
2020-10-13 15:32:49 -0500

    testing jinja

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
-using Distributed, AzManagers, Random, Test, HTTP, AzSessions, JSON
+using Distributed, AzManagers, Random, Test, HTTP, AzSessions, JSON, JinjaTemplates
+
+env = LazyTemplateLoader(["~/templates"])
 
 ss_template_json = JSON.parse(ENV["SS_TEMPLATE_JSON"])
 
@@ -22,140 +24,145 @@ template = "cbox02"
 credentials = JSON.parse(ENV["AZURE_CREDENTIALS"])
 subscriptionid = credentials["subscriptionId"]
 resourcegroup = ENV["RESOURCE_GROUP"]
+julia_version = ENV["JULIA_VERSION"]
 
 AzSessions.write_manifest(;client_id=credentials["clientId"], client_secret=credentials["clientSecret"], tenant=credentials["tenantId"])
 AzManagers.write_manifest(;resourcegroup=ENV["RESOURCE_GROUP"], subscriptionid=credentials["subscriptionId"], ssh_user="cvx")
 
-@testset "AzManagers, addprocs" for kwargs in (
-    (subscriptionid = subscriptionid, resourcegroup = resourcegroup,          ninstances = 1, group = "test$(randstring('a':'z',4))"),
-    (subscriptionid = subscriptionid, resourcegroup = resourcegroup, ppi = 2, ninstances = 2, group = "test$(randstring('a':'z',4))") )
+jinja_out = render(env, "cofii-base.json.j2"; subscriptionid=subscriptionid, resourcegroup=resourcegroup, clientId=credentials["clientId"], clientSecret=credentials["clientSecret"], tenantId=credentials["tenantId"], julia_version=julia_version)
+
+write(stdout, ">>> $jinja_out <<<")
+
+# @testset "AzManagers, addprocs" for kwargs in (
+#     (subscriptionid = subscriptionid, resourcegroup = resourcegroup,          ninstances = 1, group = "test$(randstring('a':'z',4))"),
+#     (subscriptionid = subscriptionid, resourcegroup = resourcegroup, ppi = 2, ninstances = 2, group = "test$(randstring('a':'z',4))") )
     
-    # Set up iteration vars
-    url = "https://management.azure.com/subscriptions/$subscriptionid/resourceGroups/$resourcegroup/providers/Microsoft.Compute/virtualMachineScaleSets/$(kwargs.group)?api-version=2019-12-01"
-    ninstances = kwargs.ninstances              # Number of new scale set instances to be added to the scale set
-    ppi = haskey(kwargs, :ppi) ? kwargs.ppi : 1 # Number of Julia processes to be present on each scale set instance
-    tppi = ppi*ninstances                       # Total number of Julia processes in the entire scale set
+#     # Set up iteration vars
+#     url = "https://management.azure.com/subscriptions/$subscriptionid/resourceGroups/$resourcegroup/providers/Microsoft.Compute/virtualMachineScaleSets/$(kwargs.group)?api-version=2019-12-01"
+#     ninstances = kwargs.ninstances              # Number of new scale set instances to be added to the scale set
+#     ppi = haskey(kwargs, :ppi) ? kwargs.ppi : 1 # Number of Julia processes to be present on each scale set instance
+#     tppi = ppi*ninstances                       # Total number of Julia processes in the entire scale set
 
-    # Base.structdiff(kwargs, NamedTuple{ninstances})
+#     # Base.structdiff(kwargs, NamedTuple{ninstances})
 
-    session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])
-
-
-    kwargs = (subscriptionid = kwargs.subscriptionid,
-                resourcegroup = kwargs.resourcegroup,
-                ppi = ppi,
-                group = kwargs.group,
-                session = session)
+#     session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])
 
 
-    #
-    # Unit Test 1 - Create scale set and start Julia processes
-    #
-    addprocs(template, ninstances; kwargs...)
+#     kwargs = (subscriptionid = kwargs.subscriptionid,
+#                 resourcegroup = kwargs.resourcegroup,
+#                 ppi = ppi,
+#                 group = kwargs.group,
+#                 session = session)
+
+
+#     #
+#     # Unit Test 1 - Create scale set and start Julia processes
+#     #
+#     addprocs(template, ninstances; kwargs...)
     
-    # Verify that the scale set is present
-    session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])
-    _r = HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
-    @test _r.status == 200
+#     # Verify that the scale set is present
+#     session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])
+#     _r = HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
+#     @test _r.status == 200
 
-    #
-    # Unit Test 2 - Verify that (Total # of Julia processes specified) == (Total # of Julia processes actual)
-    #
-    @test nworkers() == tppi
+#     #
+#     # Unit Test 2 - Verify that (Total # of Julia processes specified) == (Total # of Julia processes actual)
+#     #
+#     @test nworkers() == tppi
 
-    #
-    # Unit Test 3 - Verify that there is a healthy connection to each node
-    #
-    myworkers = [remotecall_fetch(gethostname, workers()[i]) for i=1:tppi]
+#     #
+#     # Unit Test 3 - Verify that there is a healthy connection to each node
+#     #
+#     myworkers = [remotecall_fetch(gethostname, workers()[i]) for i=1:tppi]
 
-    #
-    # Unit Test 4 - Verify that none of the created Julia processes are the master Julia process
-    #
-    master = gethostname()
-    unique_workers = unique(myworkers)
-    @test length(unique_workers) == ninstances
-    for worker in myworkers
-        @test master != worker
-    end
+#     #
+#     # Unit Test 4 - Verify that none of the created Julia processes are the master Julia process
+#     #
+#     master = gethostname()
+#     unique_workers = unique(myworkers)
+#     @test length(unique_workers) == ninstances
+#     for worker in myworkers
+#         @test master != worker
+#     end
 
-    #
-    # Unit Test 5 - Verify that the cloud-init startup script ran successfully
-    #
-    for i = 1:tppi
-        @test remotecall_fetch(isfile, workers()[i], ".git-credentials")
-    end
+#     #
+#     # Unit Test 5 - Verify that the cloud-init startup script ran successfully
+#     #
+#     for i = 1:tppi
+#         @test remotecall_fetch(isfile, workers()[i], ".git-credentials")
+#     end
 
-    #
-    # Unit Test 6 - Delete the Julia processes, scale set instances and the scale set itself
-    #
+#     #
+#     # Unit Test 6 - Delete the Julia processes, scale set instances and the scale set itself
+#     #
 
-    # First, verify that the scale set is present
-    session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])
-    _r = HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
-    @test _r.status == 200
+#     # First, verify that the scale set is present
+#     session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])
+#     _r = HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
+#     @test _r.status == 200
 
-    @info "Deleting cluster..."
-    rmprocs(workers())
+#     @info "Deleting cluster..."
+#     rmprocs(workers())
 
-    # Last, verify that the scale set has been deleted
-    while true
-        try
-            session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])
-            HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
-        catch _e
-            e = JSON.parse(String(_e.response.body))
-            if _e.status == 404 && e["error"]["code"] == "ResourceNotFound"
-                @info "Cluster deleted!"
-                break
-            end
-        end
-        sleep(5)
-    end
-end
-
-# Needs work - Default VM sku causes a failure here
-# @testset "AzManagers, addproc" for kwargs in ( (basename = "test$(randstring('a':'z',4))"), )
-#     addproc(template; basename=kwargs.basename)
+#     # Last, verify that the scale set has been deleted
+#     while true
+#         try
+#             session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])
+#             HTTP.request("GET", url, Dict("Authorization"=>"Bearer $(token(session))"); verbose=0)
+#         catch _e
+#             e = JSON.parse(String(_e.response.body))
+#             if _e.status == 404 && e["error"]["code"] == "ResourceNotFound"
+#                 @info "Cluster deleted!"
+#                 break
+#             end
+#         end
+#         sleep(5)
+#     end
 # end
 
-@testset "AzManagers, detach" for kwargs in ( (session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])), )
+# # Needs work - Default VM sku causes a failure here
+# # @testset "AzManagers, addproc" for kwargs in ( (basename = "test$(randstring('a':'z',4))"), )
+# #     addproc(template; basename=kwargs.basename)
+# # end
 
-    #
-    # Unit Test 1 - Create a detached job and persist the server
-    #
-    job1 = @detach vm(;session=kwargs.session, persist=true) begin
-        write(stdout, "job1 - stdout string")
-        write(stderr, "job1 - stderr string")
-    end
+# @testset "AzManagers, detach" for kwargs in ( (session = AzSession(;protocal=AzClientCredentials, client_id=credentials["clientId"], client_secret=credentials["clientSecret"])), )
 
-    #
-    # Unit Test 2 - Send a new job to the server started above
-    #
-    job2 = @detachat job1.vm["ip"] begin
-        write(stdout, "job2 - stdout string")
-        write(stderr, "job2 - stderr string")
-    end
+#     #
+#     # Unit Test 1 - Create a detached job and persist the server
+#     #
+#     job1 = @detach vm(;session=kwargs.session, persist=true) begin
+#         write(stdout, "job1 - stdout string")
+#         write(stderr, "job1 - stderr string")
+#     end
 
-    # Wait for jobs to finish
-    wait(job1)
-    wait(job2)
+#     #
+#     # Unit Test 2 - Send a new job to the server started above
+#     #
+#     job2 = @detachat job1.vm["ip"] begin
+#         write(stdout, "job2 - stdout string")
+#         write(stderr, "job2 - stderr string")
+#     end
 
-    @test status(job1) == "done"
-    @test read(job1;stdio=stdout) == "job1 - stdout string"
-    @test read(job1;stdio=stderr) == "job1 - stderr string"
+#     # Wait for jobs to finish
+#     wait(job1)
+#     wait(job2)
 
-    @test status(job2) == "done"
-    @test read(job2;stdio=stdout) == "job2 - stdout string"
-    @test read(job2;stdio=stderr) == "job2 - stderr string"
+#     @test status(job1) == "done"
+#     @test read(job1;stdio=stdout) == "job1 - stdout string"
+#     @test read(job1;stdio=stderr) == "job1 - stderr string"
 
-    #
-    # Unit Test 3 - shut-down the detached server
-    #
-    rmproc(job1.vm)
+#     @test status(job2) == "done"
+#     @test read(job2;stdio=stdout) == "job2 - stdout string"
+#     @test read(job2;stdio=stderr) == "job2 - stderr string"
 
-    #
-    # Unit Test 4 - create a new job on a new detached server that auto-destructs upon completion of its work
-    #
-    job3 = @detach vm(;session=kwargs.session, persist=false) begin
-    end
-end
+#     #
+#     # Unit Test 3 - shut-down the detached server
+#     #
+#     rmproc(job1.vm)
+
+#     #
+#     # Unit Test 4 - create a new job on a new detached server that auto-destructs upon completion of its work
+#     #
+#     job3 = @detach vm(;session=kwargs.session, persist=false) begin
+#     end
+# end
