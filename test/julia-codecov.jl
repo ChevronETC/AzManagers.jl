27ee29c3d3278e2b3e49d87f62d94cb3c4da9b7d
Matthew Chenette <matthew.chenette@chevron.com>
2020-11-04 09:18:20 -0600

    full test AGAIN

--- test/julia-codecov.jl
+++ test/julia-codecov.jl
@@ -1,7 +1,5 @@
 using Pkg
-
 Pkg.activate("coveragetempenv", shared=true)
-
 Pkg.add(PackageSpec(name="CoverageTools"))
 
 using CoverageTools
@@ -9,5 +7,4 @@ using CoverageTools
 # Usually you dont specify the path here but I had to because it kept saying file not found
 # Also I'm pretty sure this is the wrong path, it's supposed to be a package src file
 pf = process_folder("/home/cvx/.julia/packages/AzManagers/")
-
 LCOV.writefile("lcov.info", pf)
