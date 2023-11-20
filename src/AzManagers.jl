024b33ecd68b1a66dec57417997a67fbfdeff861
samkaplan <sam.kaplan@chevron.com>
2023-11-20 14:29:13 +0000

    wip

--- src/AzManagers.jl
+++ src/AzManagers.jl
@@ -1244,7 +1244,7 @@ function azure_worker_start(out::IO, cookie::AbstractString=readline(stdin); clo
 
     errormonitor(@async while isopen(sock)
         client = accept(sock)
-        ipaddr,port = getpeername(client)
+        ipaddr,__port = getpeername(client)
         cookie_from_master = read(client, Distributed.HDR_COOKIE_LEN)
         # for i = 1:10
         #     if isempty(cookie_from_master)
@@ -1255,7 +1255,7 @@ function azure_worker_start(out::IO, cookie::AbstractString=readline(stdin); clo
         #     end
         #     break
         # end
-        @info "cookie_from_master=$cookie_from_master, length(cookie_from_master)=$(length(cookie_from_master)), master_ip=$ipaddr, worker_ip=$(getipaddr()), worker_port=$port"
+        @info "cookie_from_master=$cookie_from_master, length(cookie_from_master)=$(length(cookie_from_master)), master_ip=$ipaddr, worker_ip=$(getipaddr()), worker_port=$(Distributed.LPROC.bind_port), __port=$__port"
         Distributed.process_messages(client, client, false)
     end)
     print(out, "julia_worker:")  # print header
